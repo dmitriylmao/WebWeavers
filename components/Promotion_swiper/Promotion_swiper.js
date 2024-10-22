5ec3716fecf7fddffe323b1922bae91478e0eed0
Елецкий Дмитрий <dimaeleckij2016@gmail.com>
2024-10-22 18:10:45 +0300

    feat: create promotion swiper
    пока что адаптация только для мобильных устройств и пк

---
+++ components/Promotion_swiper/Promotion_swiper.js
@@ -0,0 +1,67 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+import styles from './Promotion_swiper.module.css';
+import Button from '../Button/Button';
+
+const promotionSlide = {
+  imageSrc: '/images/Promotion_swiper/slide1.png',
+  duration: '* ЯКЦИЯ ДЕЙСТВУЕТ ДО 04/09/22',
+  title: 'Название Акции',
+  description:
+    'Условия: поставить лайк , подписаться на канал , репост на стену ВК',
+  buttonText: 'ПРИНЯТЬ УЧАСТИЕ',
+  link: '/promotion',
+};
+
+const partnerSlide = {
+  imageSrc: '/images/Promotion_swiper/slide2.png',
+  duration: 'ЯКЦИЯ ДЕЙСТВУЕТ ДО 02/03/24',
+  title: 'Наш партнер',
+  description:
+    'Леруа мерлен наш давний друг , вообще магаз норм , султан шоп советует',
+  buttonText: 'ПЕРЕЙТИ К БРЕНДУ',
+  link: '/Lerua',
+};
+
+const sliderData = [promotionSlide, partnerSlide];
+
+export default function Promotion_swiper() {
+  return (
+    <section className={styles.container}>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className={styles.mySwiper}
+      >
+        {sliderData.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className={styles.slideContent}>
+              <img
+                src={slide.imageSrc}
+                alt={slide.title}
+                className={styles.slideImage}
+              />
+              <div>
+                <p className={styles.slideDuration}>{slide.duration}</p>
+                <h2 className={styles.slideTitle}>{slide.title}</h2>
+                <p className={styles.slideDescription}>{slide.description}</p>
+                <Button
+                  customClass={styles.slideButton}
+                  text={slide.buttonText}
+                  href={slide.link}
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Translate from '../components/Translate';
+import Promotion_swiper from '../components/Promotion_swiper/Promotion_swiper';
 
 const HomePage = () => {
   return (
     <>
+      <Promotion_swiper />
       {/*<Translate /> */}
       {/* Сюда лепите ваши компоненты , формата <Component/> ,а лучше ничего не лепите , потом на ветке dev вставим , но для проверки вам в любом кайфе придется лепить , так что лепите*/}
     </>
