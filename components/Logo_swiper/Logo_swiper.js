7a8f87cd3d5cbfb8780b5e08ce8b5e6f11d6c685
Елецкий Дмитрий <dimaeleckij2016@gmail.com>
2024-10-23 20:17:22 +0300

    chore: немного другой подход для создания свайпера

--- components/Logo_swiper/Logo_swiper.js
+++ components/Logo_swiper/Logo_swiper.js
@@ -3,25 +3,24 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Pagination, Navigation } from 'swiper/modules';
+import Link from 'next/link'; 
 import styles from './Logo_swiper.module.css';
-import Image from 'next/image';
 
-// Add your image imports
-import logo1 from '../../public/images/Logo_swiper/logo1.png';
-import logo2 from '../../public/images/Logo_swiper/logo2.png';
-import logo3 from '../../public/images/Logo_swiper/logo3.png';
-import logo4 from '../../public/images/Logo_swiper/logo4.png';
-import logo5 from '../../public/images/Logo_swiper/logo5.png';
-import logo6 from '../../public/images/Logo_swiper/logo6.png';
-import logo7 from '../../public/images/Logo_swiper/logo7.png';
-import logo8 from '../../public/images/Logo_swiper/logo8.png';
-import logo9 from '../../public/images/Logo_swiper/logo9.png';
-import logo10 from '../../public/images/Logo_swiper/logo10.png';
-import logo11 from '../../public/images/Logo_swiper/logo11.png';
-import logo12 from '../../public/images/Logo_swiper/logo12.png';
-import logo13 from '../../public/images/Logo_swiper/logo13.png';
-
-// Import more logos as needed
+const logoData = [
+  { src: '/images/Logo_swiper/logo1.png', alt: 'Logo 1', name: 'Brand 1', number: '001' },
+  { src: '/images/Logo_swiper/logo2.png', alt: 'Logo 2', name: 'Brand 2', number: '002' },
+  { src: '/images/Logo_swiper/logo3.png', alt: 'Logo 3', name: 'Brand 3', number: '003' },
+  { src: '/images/Logo_swiper/logo4.png', alt: 'Logo 4', name: 'Brand 4', number: '004' },
+  { src: '/images/Logo_swiper/logo5.png', alt: 'Logo 5', name: 'Brand 5', number: '005' },
+  { src: '/images/Logo_swiper/logo6.png', alt: 'Logo 6', name: 'Brand 6', number: '006' },
+  //{ src: '/images/Logo_swiper/logo7.png', alt: 'Logo 7', name: 'Brand 7', number: '007' }//
+  { src: '/images/Logo_swiper/logo8.png', alt: 'Logo 8', name: 'Brand 8', number: '008' },
+  { src: '/images/Logo_swiper/logo9.png', alt: 'Logo 9', name: 'Brand 9', number: '009' },
+  { src: '/images/Logo_swiper/logo10.png', alt: 'Logo 10', name: 'Brand 10', number: '010' },
+  { src: '/images/Logo_swiper/logo11.png', alt: 'Logo 11', name: 'Brand 11', number: '011' },
+  { src: '/images/Logo_swiper/logo12.png', alt: 'Logo 12', name: 'Brand 12', number: '012' },
+  { src: '/images/Logo_swiper/logo13.png', alt: 'Logo 13', name: 'Brand 13', number: '013' },
+];
 
 export default function Logo_swiper() {
   return (
@@ -32,56 +31,49 @@ export default function Logo_swiper() {
       <p className={styles.subtitle}>От ведущих мировых брендов</p>
 
       <Swiper
-      
-        spaceBetween={30}
-        slidesPerView={10} 
-        slidesPerGroup={4}
+        spaceBetween={10}
+        slidesPerView={10}
+        slidesPerGroup={1}
         loop={true}
         pagination={{ clickable: true }}
         modules={[Pagination, Navigation]}
         className={styles.swiper}
+        breakpoints={{
+          320: {
+            slidesPerView: 3, 
+            spaceBetween: 10,
+          },
+          480: {
+            slidesPerView: 4, 
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 5, 
+            spaceBetween: 15,
+          },
+          768: {
+            slidesPerView: 7, 
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 8, 
+            spaceBetween: 20,
+          },
+          1400: {
+            slidesPerView: 10, 
+            spaceBetween: 20,
+          },
+        }}
       >
-        <SwiperSlide>
-          <Image src={logo1} alt="Logo 1" width={77} height={77} />
+        {logoData.map((logo, index) => (
+          <SwiperSlide key={index}>
+          <Link href={logo.number} passHref>
+            <div className={styles.card}>
+              <img src={logo.src} alt={logo.alt} className={styles.logoImage} />
+            </div>
+          </Link>
         </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo2} alt="Logo 2" width={110} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo3} alt="Logo 3" width={120} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo4} alt="Logo 4" width={130} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo5} alt="Logo 5" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo6} alt="Logo 6" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo7} alt="Logo 7" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo8} alt="Logo 8" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo9} alt="Logo 9" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo10} alt="Logo 10" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo11} alt="Logo 11" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo12} alt="Logo 12" width={100} height={77} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={logo13} alt="Logo 13" width={77} height={77} />
-        </SwiperSlide>
-        
-        {/* Add more slides as needed */}
+        ))}
       </Swiper>
     </section>
   );
