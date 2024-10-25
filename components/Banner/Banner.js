f9bbc486dee006038f3ef0698475853357da8db1
Allakhverdiev Marat <115146711+Attract1ve@users.noreply.github.com>
2024-10-25 17:43:58 +0300

    feat: create banner

---
+++ components/Banner/Banner.js
@@ -0,0 +1,25 @@
+// components/banner.js
+import React from 'react';
+import styles from './Banner.module.css';
+import Image from 'next/image';
+
+export default function Banner() {
+    return (
+        <section className={styles.bannerContainer}>
+            <img src="images/image19.png"></img> 
+               
+            <img src='images/rectangle424.png' className={styles.rect424}></img>
+            <img src='images/rectangle426.png' className={styles.rect426}></img>
+            <p className={styles.plus}>++</p>
+            
+            <p className={styles.greatDeals}> Только самые <br/>
+                                              выгодные предложения </p>
+            <p className={styles.DeliveryInfo}>Бесплатная доставка по<strong> Кокчетаву от 10 тыс ₸</strong></p>  
+
+            <p className={styles.OptomDelivery}> оптом <strong>по кокчетаву и области</strong> </p>    
+            <p className={styles.TypeOfProducts}> Бытовая химия, косметика <br/>и хозтовары </p>                         
+        </section>
+            
+        
+    )
+}
