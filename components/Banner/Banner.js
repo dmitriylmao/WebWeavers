689cc8068001ce64eb30974bf128cca7277171b3
Allakhverdiev Marat <115146711+Attract1ve@users.noreply.github.com>
2024-10-27 19:30:23 +0300

    feat: fix banner and delete non used pictures

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -6,20 +6,27 @@ import Image from 'next/image';
 export default function Banner() {
     return (
         <section className={styles.bannerContainer}>
-            <div className={styles.Img}>
-                <div className={styles.Blur}></div> 
+            <div className={styles.overlay}>
+                <div className={styles.content}>
+                    <h2 className={styles.mainTitle}>Бытовая химия, косметика и хозтовары</h2>
+                    <h3 className={styles.subTitle}>ОПТОМ ПО КОКЧЕТАВУ И ОБЛАСТИ</h3>
+                    <button className={styles.catalogButton}>В КАТАЛОГ</button>
+                    <div className={styles.features}>
+                        <div className={styles.feature}>
+                            <span className={styles.icon}>+</span>
+                            <p className={styles.featureText}>
+                                Только самые выгодные предложения
+                            </p>
+                        </div>
+                        <div className={styles.feature}>
+                            <span className={styles.icon}>+</span>
+                            <p className={styles.featureText}>
+                                Бесплатная доставка по <span className={styles.highlights}>Кокчетаву от 10 тыс ₸</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
-            <img src='images/rectangle424.png' className={styles.rect424}></img>
-            <img src='images/rectangle426.png' className={styles.rect426}></img>
-            <p className={styles.plus}>++</p>
-            
-            <p className={styles.greatDeals}> Только самые <br/>
-                                              выгодные предложения </p>
-            <p className={styles.DeliveryInfo}>Бесплатная доставка по<strong> Кокчетаву от 10 тыс ₸</strong></p>  
-
-            <p className={styles.OptomDelivery}> оптом <strong>по кокчетаву и области</strong> </p>    
-            <p className={styles.TypeOfProducts}> Бытовая химия, косметика <br/>и хозтовары </p>                         
         </section>
             
         
