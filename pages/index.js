1c99137224366b361ca0ed882ab0b71276c10995
Quant1co <115133527+Quant1co@users.noreply.github.com>
2024-10-30 22:54:42 +0300

    feat: create header
    Create all functional for header

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Translate from '../components/Translate';
 import Promotion_swiper from '../components/Promotion_swiper/Promotion_swiper';
 import Categories from '../components/Сategories/Сategories';
+import { Header } from '@/components/Header/Header';
 
 const HomePage = () => {
   return (
     <>
+      <Header/>
       <Categories />
       <Promotion_swiper />
+      
       {/* Сюда лепите ваши компоненты , формата <Component/> ,а лучше ничего не лепите , потом на ветке dev вставим , но для проверки вам в любом кайфе придется лепить , так что лепите*/}
     </>
   );
