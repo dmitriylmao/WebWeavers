4f631779aff2d19098cd9da9ae62920dff5b1ff6
Елецкий Дмитрий <dimaeleckij2016@gmail.com>
2024-10-31 17:24:15 +0300

    chore: npm run format , for a successful build

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Translate from '../components/Translate';
 import Promotion_swiper from '../components/Promotion_swiper/Promotion_swiper';
 import Categories from '../components/Сategories/Сategories';
 import { Header } from '@/components/Header/Header';
