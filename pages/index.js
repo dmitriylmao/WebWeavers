42d06ce828f4edd4791972ade112e57f740569a4
Елецкий Дмитрий <dimaeleckij2016@gmail.com>
2024-10-31 17:21:16 +0300

    chore: npm run format , for a successful build

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ import { Header } from '@/components/Header/Header';
 const HomePage = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <Categories />
       <Promotion_swiper />
-      
+
       {/* Сюда лепите ваши компоненты , формата <Component/> ,а лучше ничего не лепите , потом на ветке dev вставим , но для проверки вам в любом кайфе придется лепить , так что лепите*/}
     </>
   );
