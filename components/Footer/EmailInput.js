9507ee2d36f1b6edf731cf2e47e59a1e57992dda
Елецкий Дмитрий <dimaeleckij2016@gmail.com>
2024-11-13 21:44:03 +0300

    chore: improve code

--- components/Footer/EmailInput.js
+++ components/Footer/EmailInput.js
@@ -1,17 +1,16 @@
-import React from 'react';
 import styles from './EmailInput.module.css';
 
 export default function EmailInput() {
-    return (
-        <div className={styles.container}>
-            <input
-                type="email"
-                placeholder="Введите ваш E-mail"
-                className={styles.input}
-            />
-            <button className={styles.button}>
-                <span>&#9654;</span>
-            </button>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <input
+        type="email"
+        placeholder="Введите ваш E-mail"
+        className={styles.input}
+      />
+      <button className={styles.button}>
+        <span>&#9654;</span>
+      </button>
+    </div>
+  );
 }
