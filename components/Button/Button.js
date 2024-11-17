0d01d81c4268c7c7bef5e45f6fe1aae7237915b1
Елецкий Дмитрий <dimaeleckij2016@gmail.com>
2024-11-17 14:07:24 +0300

    Update Button.js

---
+++ components/Button/Button.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from './Button.module.css'; 
+
+const Button = ({ text, href, imgSrc, customClass }) => {
+  return (
+    <Link href={href} className={`${styles.button} ${customClass || ''}`}>
+      {text}
+      {imgSrc && (
+        <img src={imgSrc} alt="button icon" className={styles.buttonImage} />
+      )}
+    </Link>
+  );
+};
+
+export default Button;
