811a421cb09643c64285610e0183b2b8226d6d8b
CutePufik <ad.cherevko@gmail.com>
2024-12-03 20:51:08 +0300

    corrected behavior Description component

--- components/Product/Description/Description.js
+++ components/Product/Description/Description.js
@@ -19,19 +19,19 @@ const Description = ({ text }) => {
             <h2 onClick={toggleDescription} className={style.descriptionTitle}>
                 {t('Description')}
                 <motion.span
-                    initial={{ rotate: 0 }}
-                    animate={{ rotate: isOpen ? 240 : 0 }}
-                    transition={{ duration: 0.5, ease: 'easeInOut' }}
+                    initial={{rotate: 0}}
+                    animate={{rotate: isOpen ? 240 : 0}}
+                    transition={{duration: 0.5, ease: 'easeInOut'}}
                 >
                     {isOpen ? '▴' : '▾'}
                 </motion.span>
             </h2>
             <motion.div
                 className={style.descriptionContainer}
-                style={{ maxHeight }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: isOpen ? 1 : 0 }}
-                transition={{ duration: 0.6, ease: 'easeInOut' }}
+                style={{maxHeight}}
+                initial={{opacity: 0}}
+                animate={{opacity: isOpen ? 1 : 0}}
+                transition={{duration: 0.6, ease: 'easeInOut'}}
             >
                 <p ref={contentRef} className={style.descriptionText}>
                     {text}
